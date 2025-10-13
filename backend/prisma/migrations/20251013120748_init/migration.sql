a049289a8c1696f57eb62397afe256f3f36df941
Chiwawa913 <mortira919@gmail.com>
2025-10-13 18:03:12 +0500

    fix: finally resolve database migrations

--- backend/prisma/migrations/20251013120748_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "firebaseUid" TEXT NOT NULL PRIMARY KEY,
-    "telegramChatId" TEXT NOT NULL,
-    "timezone" TEXT NOT NULL DEFAULT 'UTC'
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_telegramChatId_key" ON "User"("telegramChatId");
